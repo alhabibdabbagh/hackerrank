9ca511a2f9bab465375b80f1355afdf45095c05e
alhabibdabbagh <habidabbagh@gmail.com>
2021-04-29 01:29:39 +0300

    second question ConvertFromIntegerToString

--- src/com/project/Main.java
+++ src/com/project/Main.java
@@ -11,25 +11,11 @@ import java.util.Scanner;
  */
 public class Main {
 
-    public  static  int B;
-    public  static  int H;
-    public  static  boolean flag;
     public  static Scanner ss=new Scanner(System.in);
-static {
-    if(B>0 && H>0){
-        flag=true;
-    }else {
-        System.out.println("java.lang.Exception: Breadth and height must be positive");
-    }
-}
 
     public static void main(String[] args) {
 
-        initializeClassVariable();
-        if(flag){
-            int area=B*H;
-            System.out.print(area);
-        }
+
 
         Integer ssa = 0;
         String ww=ssa.toString();
@@ -45,15 +31,7 @@ static {
     }
 
 
-    private static void initializeClassVariable() {
-        B=ss.nextInt();
-        H=ss.nextInt();
-        if(B>0 && H>0){
-            flag=true;
-        }else {
-            System.out.println("java.lang.Exception: Breadth and height must be positive");
-        }
-    }
+
 
       /*  Scanner s = new Scanner(System.in);
 
---
+++ src/com/project/hackerrank/second/ConvertFromIntegerToString.java
@@ -0,0 +1,27 @@
+package com.project.hackerrank.second;
+
+import java.util.Scanner;
+
+/**
+ * @author habib
+ * @on 29.04.2021 01:28
+ */
+public class ConvertFromIntegerToString {
+    public  static Scanner ss=new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+        Integer ssa = 0;
+        String ww=ssa.toString();
+
+    }
+
+
+    public  static  String s;
+    public  static  int sd=ss.nextInt();
+
+    static {
+        s=String.valueOf(ss);
+    }
+
+}
