fcea12270b7506c9150c6a152b46d9b45e36f90f
alhabibdabbagh <habidabbagh@gmail.com>
2021-05-01 01:57:50 +0300

    currency fourth question resolved

--- src/com/project/hackerrank/fourth/Currency.java
+++ src/com/project/hackerrank/fourth/Currency.java
@@ -1,14 +1,28 @@
 package com.project.hackerrank.fourth;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  * @author habib
  * @on 29.04.2021 02:23
  */
 public class Currency {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
 
-       
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india = NumberFormat.getCurrencyInstance(new Locale("en", "in")).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+        // Write your code here.
 
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
     }
-
 }
